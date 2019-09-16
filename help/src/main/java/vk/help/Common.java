c714cd45571c34dc3bc75c5dfd837a9949c510db
Vikrant Kaloniya <kaloniya124001@gmail.com>
2019-09-16 10:37:37 +0530

    add a common class for common task

--- help/src/main/java/vk/help/Common.java
+++ help/src/main/java/vk/help/Common.java
@@ -1,4 +1,180 @@
 package vk.help;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.StrictMode;
+import android.util.Base64;
+import android.view.View;
+
+import com.google.gson.Gson;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Type;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Common {
+
+    public static final String OnlyDatePattern = "dd-MMM-yyyy";
+    public static final String ServerCommonDateTimePattern = "yyyy-MM-dd'T'HH:mm:ss";
+
+    public static String convertDate(String from, String to, String value) {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat(from.isEmpty() ? ServerCommonDateTimePattern : from, Locale.getDefault());
+            Date sourceDate = dateFormat.parse(value);
+            SimpleDateFormat targetFormat = new SimpleDateFormat(to.isEmpty() ? OnlyDatePattern : to, Locale.getDefault());
+            if (sourceDate != null) {
+                value = targetFormat.format(sourceDate);
+            } else {
+                return value;
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+    public static String capitalize(String str) {
+        str = str.toLowerCase();
+        String[] words = str.trim().split(" ");
+        StringBuilder ret = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].trim().length() > 0) {
+                ret.append(Character.toUpperCase(words[i].trim().charAt(0)));
+                ret.append(words[i].trim().substring(1));
+                if (i < words.length - 1) {
+                    ret.append(' ');
+                }
+            }
+        }
+        return ret.toString();
+    }
+
+    public static void setEnableViews(boolean value, View... views) {
+        for (View view : views) {
+            view.setEnabled(value);
+        }
+    }
+
+    public static String getJSON(Object o) {
+        return new Gson().toJson(o);
+    }
+
+    public static Object getObject(String json, Type type) throws NullPointerException {
+        return new Gson().fromJson(json, type);
+    }
+
+    public static byte[] getBytes(Object o) {
+        byte[] bytes = null;
+        ObjectOutput out;
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+            out = new ObjectOutputStream(bos);
+            out.writeObject(o);
+            out.flush();
+            bytes = bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bytes;
+    }
+
+    public static Object getObject(byte[] bytes) {
+        Object o = null;
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        try (ObjectInput in = new ObjectInputStream(bis)) {
+            o = in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return o;
+    }
+
+    public static String getBase64(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        byte[] imageBytes = outputStream.toByteArray();
+        return Base64.encodeToString(imageBytes, Base64.DEFAULT);
+    }
+
+    public static Intent openFile(File url) {
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        //StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+        Uri uri = Uri.fromFile(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        if (url.toString().contains(".doc") || url.toString().contains(".docx")) {
+            // Word document
+            intent.setDataAndType(uri, "application/msword");
+        } else if (url.toString().contains(".pdf")) {
+            // PDF file
+            intent.setDataAndType(uri, "application/pdf");
+        } else if (url.toString().contains(".ppt") || url.toString().contains(".pptx")) {
+            // Powerpoint file
+            intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
+        } else if (url.toString().contains(".xls") || url.toString().contains(".xlsx")) {
+            // Excel file
+            intent.setDataAndType(uri, "application/vnd.ms-excel");
+        } else if (url.toString().contains(".zip") || url.toString().contains(".rar")) {
+            // WAV audio file
+            intent.setDataAndType(uri, "application/x-wav");
+        } else if (url.toString().contains(".rtf")) {
+            // RTF file
+            intent.setDataAndType(uri, "application/rtf");
+        } else if (url.toString().contains(".wav") || url.toString().contains(".mp3")) {
+            // WAV audio file
+            intent.setDataAndType(uri, "audio/x-wav");
+        } else if (url.toString().contains(".gif")) {
+            // GIF file
+            intent.setDataAndType(uri, "image/gif");
+        } else if (url.toString().contains(".jpg") || url.toString().contains(".jpeg") || url.toString().contains(".png")) {
+            // JPG file
+            intent.setDataAndType(uri, "image/jpeg");
+        } else if (url.toString().contains(".txt")) {
+            // Text file
+            intent.setDataAndType(uri, "text/plain");
+        } else if (url.toString().contains(".3gp") || url.toString().contains(".mpg") || url.toString().contains(".mpeg") || url.toString().contains(".mpe") || url.toString().contains(".mp4") || url.toString().contains(".avi")) {
+            // Video files
+            intent.setDataAndType(uri, "video/*");
+        } else {
+            intent.setDataAndType(uri, "*/*");
+        }
+        //     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return intent;
+    }
+
+    public static String nameFromURL(String url) {
+        try {
+            return url != null ? url.substring(url.lastIndexOf('/') + 1) : "";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static String getFileSize(long size) {
+        DecimalFormat df = new DecimalFormat("0.00");
+        float sizeKb = 1024.0f;
+        float sizeMb = sizeKb * sizeKb;
+        float sizeGb = sizeMb * sizeKb;
+        if (size < sizeKb) {
+            return df.format(1) + " Kb";
+        } else if (size < sizeMb)
+            return df.format(size / sizeKb) + " Kb";
+        else if (size < sizeGb)
+            return df.format(size / sizeMb) + " Mb";
+        return "";
+    }
+
 }
---
+++ help/src/main/java/vk/help/MasterActivity.java
@@ -0,0 +1,18 @@
+package vk.help;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MasterActivity extends AppCompatActivity {
+
+    public Context context;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = this;
+    }
+}
