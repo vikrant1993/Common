7655e458e17d9d72cd17c1179abda0c6028a8059
Vikrant Kaloniya <kaloniya124001@gmail.com>
2019-09-17 11:03:35 +0530

    add a common class for common task

--- app/src/main/java/vk/help/common/MainActivity.kt
+++ app/src/main/java/vk/help/common/MainActivity.kt
@@ -1,18 +1,16 @@
 package vk.help.common
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import vk.help.MasterActivity
 import vk.help.network.NetworkRequest
 import vk.help.network.NetworkResponse
 import vk.help.network.ResultsListener
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : MasterActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         NetworkRequest(null, null, object : ResultsListener {
             override fun onResultsSucceeded(result: NetworkResponse) {
 
--- help/src/main/java/vk/help/MasterActivity.kt
+++ help/src/main/java/vk/help/MasterActivity.kt
@@ -1,15 +1,26 @@
 package vk.help
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 open class MasterActivity : AppCompatActivity() {
+
+    companion object {
+        const val DATA = "DATA"
+        const val KEY = "KEY"
+        const val ID = "ID"
+    }
 
     public lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         context = this
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
