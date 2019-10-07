26f50aa58cf732b1d45764c8ad3dc4cb68545102
Vikrant Kaloniya <kaloniya124001@gmail.com>
2019-10-07 10:56:29 +0530

    add a common class for common task

--- app/src/main/java/vk/help/common/utility/Constants.java
+++
@@ -1,9 +0,0 @@
-package vk.help.common.utility;
-
-public class Constants {
-
-    public static final String PARAMETER_SEP = "&";
-    public static final String PARAMETER_EQUALS = "=";
-    public static final String TRANS_URL = "https://secure.ccavenue.com/transaction/initTrans";
-
-}
