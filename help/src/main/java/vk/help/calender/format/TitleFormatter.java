dfc7fcb1a6c20e5b059307a4870f31938444182d
Vikrant-MIPL <vikrant.k@mishainfotech.com>
2019-12-11 11:51:52 +0530

    add a common class for common task

--- help/src/main/java/vk/help/calender/format/TitleFormatter.java
+++ help/src/main/java/vk/help/calender/format/TitleFormatter.java
@@ -3,20 +3,11 @@ package vk.help.calender.format;
 
 import vk.help.calender.CalendarDay;
 
-/**
- * Used to format a {@linkplain CalendarDay} to a string for the month/year title
- */
 public interface TitleFormatter {
 
     String DEFAULT_FORMAT = "LLLL yyyy";
 
     TitleFormatter DEFAULT = new DateFormatTitleFormatter();
 
-    /**
-     * Converts the supplied day to a suitable month/year title
-     *
-     * @param day the day containing relevant month and year information
-     * @return a label to display for the given month/year
-     */
     CharSequence format(CalendarDay day);
 }
