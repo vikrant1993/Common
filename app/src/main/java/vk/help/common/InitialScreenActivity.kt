e95e50fbfbded5b387e3849e5f8c1b6e5353a92d
Vikrant-MIPL <vikrant.k@mishainfotech.com>
2019-12-28 14:47:19 +0530

    add a common class for common task

--- app/src/main/java/vk/help/common/InitialScreenActivity.kt
+++ app/src/main/java/vk/help/common/InitialScreenActivity.kt
@@ -1,10 +1,8 @@
 package vk.help.common
 
-import android.content.Intent
 import android.os.Bundle
 import kotlinx.android.synthetic.main.temp.*
 import vk.help.MasterActivity
-import vk.help.placepicker.ActivityPlacePicker
 
 class InitialScreenActivity : MasterActivity() {
 
@@ -12,7 +10,7 @@ class InitialScreenActivity : MasterActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.temp)
         updateDate.setOnClickListener {
-            startActivity(Intent(context, ActivityPlacePicker::class.java))
+//            startActivity(Intent(context, ActivityPlacePicker::class.java))
         }
     }
 }
