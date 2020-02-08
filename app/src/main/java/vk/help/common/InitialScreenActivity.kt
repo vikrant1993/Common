87c4ad2767257fc25875a768461add521898e08a
Vikrant-MIPL <vikrant.k@mishainfotech.com>
2020-02-08 17:45:52 +0530

    add a common class for common task

--- app/src/main/java/vk/help/common/InitialScreenActivity.kt
+++ app/src/main/java/vk/help/common/InitialScreenActivity.kt
@@ -1,8 +1,12 @@
 package vk.help.common
 
+import android.content.Intent
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.temp.*
 import vk.help.MasterActivity
+import vk.help.imagepicker.features.ImagePicker
+import java.io.File
 
 class InitialScreenActivity : MasterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +16,17 @@ class InitialScreenActivity : MasterActivity() {
         imageView.setImage("https://image.shutterstock.com/image-photo/colorful-flower-on-dark-tropical-260nw-721703848.jpg")
 
         updateDate.setOnClickListener {
-
+            ImagePicker.create(this).single().showCamera(true).start()
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
+            val images = ImagePicker.getImages(data)
+            if (images.size > 0) {
+                Glide.with(context).load(File(images[0].path)).into(imageView)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
