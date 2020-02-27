d96e8bb689c4460cc6f451e7aed68bf8d10ac9f0
Vikrant-MIPL <vikrant.k@mishainfotech.com>
2020-02-27 11:19:53 +0530

    add a common class for common task

--- help/src/main/java/vk/help/calender/EventComparator.kt
+++ help/src/main/java/vk/help/calender/EventComparator.kt
@@ -1,7 +1,5 @@
 package vk.help.calender
 
-import java.util.*
-
 class EventComparator : Comparator<Event> {
     override fun compare(lhs: Event, rhs: Event): Int {
         return lhs.timeInMillis.compareTo(rhs.timeInMillis)
