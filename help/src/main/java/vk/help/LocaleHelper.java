dc6d790033583d884907df7ee588bd50f7c96421
Vikrant-MIPL <kaloniya124001@gmail.com>
2020-05-13 18:30:35 +0530

    update

--- help/src/main/java/vk/help/LocaleHelper.java
+++ help/src/main/java/vk/help/LocaleHelper.java
@@ -30,11 +30,9 @@ public class LocaleHelper {
 
     public static Context setLocale(Context context, String language) {
         persist(context, language);
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             return updateResources(context, language);
         }
-
         return updateResourcesLegacy(context, language);
     }
 
@@ -46,7 +44,6 @@ public class LocaleHelper {
     private static void persist(Context context, String language) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = preferences.edit();
-
         editor.putString(SELECTED_LANGUAGE, language);
         editor.apply();
     }
